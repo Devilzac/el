420f9f1593ec9559686bf3f0f741af436c2748c7
Devilzac <shane199274@gmail.com>
2019-12-26 13:34:20 +0100

    First commit. added axios

---
+++ src/components/item.vue
@@ -0,0 +1,39 @@
+<template>
+   <div>
+     <table>
+       <tbody>
+         <tr>
+           <td>hola</td>
+         </tr>
+       </tbody>
+     </table>
+   </div>
+</template>
+
+<script>
+import Axios from 'axios';
+
+export default {
+  data() {
+    return {
+      titulo: null,
+    };
+  },
+  mounted() {
+    this.getInfo();
+  },
+  methods: {
+    getInfo() {
+      Axios.get('https://cat-fact.herokuapp.com').then((response) => {
+        console.log('response: ', response);
+      }).catch((e) => {
+        console.log(e);
+      });
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <item msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import item from '@/components/item.vue';
+
+export default {
+  name: 'home',
+  components: {
+    item,
+  },
+};
+</script>
